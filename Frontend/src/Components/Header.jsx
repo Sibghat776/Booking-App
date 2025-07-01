f85039190e7394e0c219567218d812e0c85d2d0e
Sibghat776 <ullahsibghat786@gmail.com>
2025-07-01 17:40:40 +0500

    OptionsCounter Succesfuly running

--- Frontend/src/Components/Header.jsx
+++ Frontend/src/Components/Header.jsx
@@ -1,4 +1,4 @@
-import react from 'react';
+import react, { Children } from 'react';
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import { styled } from '@mui/material/styles';
@@ -8,7 +8,7 @@ import FlightIcon from '@mui/icons-material/Flight';
 import LocalTaxiIcon from '@mui/icons-material/LocalTaxi';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import Box from "@mui/material"
+import { Box } from "@mui/material"
 import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css';
 import TextField from '@mui/material/TextField';
@@ -16,6 +16,8 @@ import { Calendar, DateRangePicker } from 'react-date-range';
 import React, { useState } from 'react';
 import { addDays } from 'date-fns';
 import { format } from 'date-fns';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import GroupIcon from '@mui/icons-material/Group';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: '#fff',
@@ -38,12 +40,28 @@ export default function Header() {
             key: 'selection'
         }
     ]);
+    const [options, setOptions] = useState(
+        {
+            adult: 1,
+            children: 0,
+            room: 1
+        }
+    )
+    const [openOptions, setOpenOptions] = useState(false)
+    const handleOption = (name, operation) => {
+        setOptions((prev) => {
+            return {
+                ...prev, [name]: operation === "i" ? options[name] + 1 : options[name] - 1
+            }
+        });
+    }
+
     return (
         <div className='max-w-screen bg-[#132a13] mx-auto px-16 pt-2'>
             <Stack
                 direction={{ xs: 'column', sm: 'row' }}
                 spacing={{ xs: 1, sm: 2, md: 2 }}
-                className='w-auto pt-4'
+                className='w-screen-sm px-40 pt-4'
             >
                 <Item style={{ backgroundColor: "transparent", borderRadius: "20px", color: "#F7FFF4", border: "1px solid #F7FFF4", display: "flex", fontSize: "0.8rem", justifyContent: "center", alignItems: "center", gap: "0.5rem", padding: "10px" }}><HotelIcon style={{ fontSize: "1.2rem" }} />Stays</Item>
                 <Item style={{ backgroundColor: "transparent", boxShadow: "none", color: "#F7FFF4", display: "flex", fontSize: "0.8rem", justifyContent: "center", alignItems: "center", gap: "0.5rem", padding: "10px" }}><FlightIcon style={{ fontSize: "1.2rem" }} />Flights</Item>
@@ -52,19 +70,18 @@ export default function Header() {
                 <Item style={{ backgroundColor: "transparent", boxShadow: "none", color: "#F7FFF4", display: "flex", fontSize: "0.8rem", justifyContent: "center", alignItems: "center", gap: "0.5rem", padding: "10px" }}><LocalTaxiIcon style={{ fontSize: "1.2rem" }} />Airport Taxi</Item>
             </Stack>
 
-            <Typography variant="h3" color='#F7FFF4' paddingY={"10px"} component="h2">
+            <Typography variant="h3" color='#F7FFF4' paddingX={"160px"} paddingY={"10px"} component="h2">
                 A lifetime of discount? It's Genius.
             </Typography>
 
-            <Typography variant="p" color='#F7FFF4' paddingBottom={"10px"} component="p">
+            <Typography variant="p" color='#F7FFF4' paddingX={"160px"} paddingBottom={"10px"} component="p">
                 Get rewarded fro your travels - unlock instant savings of 10% or more with a free Sibghat booking account.
             </Typography>
-            <Button variant="contained" style={{ backgroundColor: "#31572c", margin: "35px 0px" }}>Sign in / Register</Button>
-
+            <Button variant="contained" style={{ backgroundColor: "#31572c", margin: "35px 160px" }}>Sign in / Register</Button>
 
             <Stack direction="row" className='bg-[#ecf39e] top-10 relative w-auto items-center justify-center p-1 py-2 border-[#90a955] border-4' spacing={2}>
                 <Item style={{ display: "flex", justifyContent: "center", width: "30%", fontSize: "1.01rem", boxShadow: "none", height: "100%", color: "#132a13", alignItems: "center", gap: "16px", backgroundColor: "transparent" }}><HotelIcon /><TextField variant='standard' className='border-none' placeholder='Where are you going?' /></Item>
-                <Item className='relative' style={{ backgroundColor: "transparent", width: "30%", fontSize: "1.01rem", boxShadow: "none", height: "100%", color: "#132a13", cursor: "pointer" }} onClick={() => { setOpenDate(!openDate) }}>{`${format(date[0].startDate, "dd/MM/yyyy")} to ${format(date[0].endDate, "dd/MM/yyyy")}`}
+                <Item className='relative' style={{ backgroundColor: "transparent", width: "30%", fontSize: "1.01rem", boxShadow: "none", height: "100%", color: "#132a13", cursor: "pointer" }} onClick={() => { setOpenDate(!openDate) }}><CalendarMonthIcon className='mx-4' />{`${format(date[0].startDate, "dd/MM/yyyy")} to ${format(date[0].endDate, "dd/MM/yyyy")}`}
                     {openDate && <DateRangePicker
                         className='dateRange'
                         onChange={item => setDate([item.selection])}
@@ -74,25 +91,26 @@ export default function Header() {
                         ranges={date}
                     />}
                 </Item>
-                <Item style={{ position: "relative", backgroundColor: "transparent", fontSize: "1.01rem",cursor: "pointer", width: "30%", boxShadow: "none", height: "100%", color: "#132a13" }}>2 Adults 2 Children 1 room</Item>
-                <Box>
-                    <Stack className='absolute bottom-[-40px] right-0' direction={'row'} justifyContent={'center'} alignItems={'center'} gap={"10px"}>
-                    <Button variant="contained" style={{ backgroundColor: "#132a13" }}>+</Button>
-                    <Typography>1</Typography>
-                    <Button variant="contained" style={{ backgroundColor: "#132a13" }}>+</Button>
-                </Stack>
-                    <Stack className='absolute bottom-[-40px] right-0' direction={'row'} justifyContent={'center'} alignItems={'center'} gap={"10px"}>
-                    <Button variant="contained" style={{ backgroundColor: "#132a13" }}>+</Button>
-                    <Typography>0</Typography>
-                    <Button variant="contained" style={{ backgroundColor: "#132a13" }}>+</Button>
-                </Stack>
-                    <Stack className='absolute bottom-[-40px] right-0' direction={'row'} justifyContent={'center'} alignItems={'center'} gap={"10px"}>
-                    <Button variant="contained" style={{ backgroundColor: "#132a13" }}>+</Button>
-                    <Typography>1</Typography>
-                    <Button variant="contained" style={{ backgroundColor: "#132a13" }}>+</Button>
-                </Stack>
+                <Item onClick={() => { setOpenOptions(!openOptions) }} style={{ position: "relative", backgroundColor: "transparent", fontSize: "1.01rem", cursor: "pointer", width: "30%", boxShadow: "none", height: "100%", color: "#132a13" }}><GroupIcon className='mx-4' />{`${options.adult} Adult ${options.children} Children  ${options.room} Room`}</Item>
+                {openOptions && <Box className='absolute top-[65px] flex flex-col justify-center items-center gap-4 bg-[#4f772d] text-[#ecf39e] p-2 border-[#90a955] border-2 right-[320px]'>
+                    <Stack direction={'row'} justifyContent={'center'} alignItems={'center'} gap={"10px"}>
+                        <Button disabled={options.adult <= 1} variant="contained" style={{ backgroundColor: "#132a13" }} onClick={() => handleOption("adult", "d")}>-</Button>
+                        <Typography>{options.adult}</Typography>
+                        <Button variant="contained" style={{ backgroundColor: "#132a13" }} onClick={() => handleOption("adult", "i")}>+</Button>
+                    </Stack>
+                    <Stack direction={'row'} justifyContent={'center'} alignItems={'center'} gap={"10px"}>
+                        <Button disabled={options.children <= 0} variant="contained" style={{ backgroundColor: "#132a13" }} onClick={() => handleOption("children", "d")}>-</Button>
+                        <Typography>{options.children}</Typography>
+                        <Button variant="contained" style={{ backgroundColor: "#132a13" }} onClick={() => handleOption("children", "i")}>+</Button>
+                    </Stack>
+                    <Stack direction={'row'} justifyContent={'center'} alignItems={'center'} gap={"10px"}>
+                        <Button disabled={options.room <= 1} variant="contained" style={{ backgroundColor: "#132a13" }} onClick={() => handleOption("room", "d")}>-</Button>
+                        <Typography>{options.room}</Typography>
+                        <Button variant="contained" style={{ backgroundColor: "#132a13" }} onClick={() => handleOption("room", "i")}>+</Button>
+                    </Stack>
                 </Box>
-                <Button variant="contained" color="success" style={{ marginRight: "19px" }}>Success</Button>
+                }
+                <Button variant="contained" color="success" style={{ marginRight: "19px" }}>Search</Button>
             </Stack>
         </div>
     );
